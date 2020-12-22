b225609172c2ee1526d4672a1830211365a67338
Thang <thangnus@gmail.com>
2020-12-23 00:09:17 +1300

    Refactor and Add Topic Exchange

--- node-src/src/consumer.ts
+++ node-src/src/consumer.ts
@@ -1,7 +1,10 @@
 import { ConsumeMessage } from "amqplib"
-import { OrderTaxiProp } from "./publisher"
+import { OrderTaxiDirectProp, OrderTaxiByTopicProp } from "./publisher"
 
-export async function taxiSubscribe({ channel, queue }: OrderTaxiProp) {
+export async function taxiSubscribeDirect({
+  channel,
+  queue,
+}: OrderTaxiDirectProp) {
   function processOrder(message: ConsumeMessage | null) {
     if (!message) return
     try {
@@ -12,6 +15,30 @@ export async function taxiSubscribe({ channel, queue }: OrderTaxiProp) {
       channel.nack(message)
     }
   }
+  await channel.consume(queue.queue, processOrder, {
+    noAck: false,
+  })
+}
+
+export async function taxiSubscribeByTopic({
+  channel,
+  key,
+  exchange,
+  queue,
+}: OrderTaxiByTopicProp) {
+  function processOrder(message: ConsumeMessage | null) {
+    if (!message) return
+    try {
+      console.log(`Processing order ${message.content}, which can fail!`)
+      channel.ack(message)
+    } catch (error) {
+      console.error(error.message)
+      channel.nack(message)
+    }
+  }
+
+  await channel.bindQueue(queue.queue, exchange.exchange, key)
+
   await channel.consume(queue.queue, processOrder, {
     noAck: false,
   })
--- node-src/src/publisher.ts
+++ node-src/src/publisher.ts
@@ -3,10 +3,17 @@ import { Buffer } from "buffer"
 import { v4 as uuidv4 } from "uuid"
 import { declareQueue } from "./declareQueue"
 
-export type OrderTaxiProp = {
+export type OrderTaxiDirectProp = {
   channel: amqp.Channel
   queue: amqp.Replies.AssertQueue
-  directExchange: amqp.Replies.AssertExchange
+  exchange: amqp.Replies.AssertExchange
+}
+
+export type OrderTaxiByTopicProp = {
+  channel: amqp.Channel
+  exchange: amqp.Replies.AssertExchange
+  key: string
+  queue: amqp.Replies.AssertQueue
 }
 
 class OrderTaxiMessage {
@@ -21,22 +28,42 @@ class OrderTaxiMessage {
   }
 }
 
-export async function orderTaxi({
+export async function orderTaxiDirect({
   channel,
   queue,
-  directExchange,
-}: OrderTaxiProp) {
+  exchange,
+}: OrderTaxiDirectProp) {
   // re-declare to make sure queue exists (idempotent action)
-  const taxiQueue = await declareQueue({ channel, taxiName: queue.queue })
+  const taxiQueue = (await declareQueue({ channel, taxiName: queue.queue }))
+    .queue
+
   setInterval(
     () =>
       // server-push to reduce load as opposed to front-end polling
       channel.publish(
-        directExchange.exchange,
-        taxiQueue.queue,
+        exchange.exchange,
+        taxiQueue,
         Buffer.from(
-          new OrderTaxiMessage(`Sending to taxi ${queue.queue}`).toString()
+          new OrderTaxiMessage(`Sending to taxi ${taxiQueue}`).toString()
         ),
+        { messageId: uuidv4(), persistent: true }
+      ),
+    500
+  )
+}
+
+export async function orderTaxiByTopic({
+  channel,
+  key,
+  exchange,
+}: OrderTaxiByTopicProp) {
+  setInterval(
+    () =>
+      // server-push to reduce load as opposed to front-end polling
+      channel.publish(
+        exchange.exchange,
+        key,
+        Buffer.from(new OrderTaxiMessage(`Sending to taxi ${key}`).toString()),
         { messageId: uuidv4(), persistent: true }
       ),
     500
